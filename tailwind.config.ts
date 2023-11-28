051ef8a40d7e2525c16fa5086e2dcedae84ca9cf
sunaookamishirokodev <lethanhtrung.trungle@gmail.com>
2023-11-28 11:15:25 +0700

    fix not-found

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,11 @@
 import type { Config } from "tailwindcss";
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  plugins: [
-    plugin(function ({ matchUtilities, theme }: {matchUtilities: any, theme: any}) {
-      matchUtilities(
-        {
-          "translate-z": (value: any) => ({
-            "--tw-translate-z": value,
-            transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
-          }),
-        },
-        { values: theme("translate"), supportsNegativeValues: true }
-      );
-    }),
   ],
 };
 export default config;
